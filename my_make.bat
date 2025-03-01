a3c032bf6c9052ace14f482014985e01cf3211e0
tomsons26 <tomsons26lv@gmail.com>
2025-03-01 20:08:56 +0200

    gets win32lib to compile

--- my_make.bat
+++ my_make.bat
@@ -1,5 +1,7 @@
 SET ROOT=%cd%
 
+SET PATH=%windir%;%windir%\System32
+
 REM Compilers and utilities
 SET WATCOM=D:\WATCOM
 SET TASM=D:\Projects\TASM
@@ -9,11 +11,11 @@ SET MASM=D:\MASM_6_11D
 REM Define Westwood libraries
 SET WIN32LIB=%ROOT%\WIN32LIB
 :: doesn't actually matter, needs to be removed from makefiles
-SET WIN32VCS=%ROOT%\WIN32LIB
+SET WIN32VCS=%WIN32LIB%
 
 REM 3rd party libraries
 
-SET PATH=%WATCOM%\BINNT;%WATCOM%\H;%WATCOM%\H\NT;%PATH%
+SET PATH=%WATCOM%\BINNT;%WATCOM%\BIN;%WATCOM%\BINW;%WATCOM%\H;%WATCOM%\H\NT;%PATH%
 SET PATH=%TASM%\BIN;%PATH%
 
 md OBJ\WIN32\ENGLISH
@@ -25,6 +27,7 @@ md RUN\GERMAN
 
 md WINVQ\VQM32\O
 md WINVQ\VQA32\O
+md WIN32LIB\LIB
 
 ::copy /Y %IPX%\IPXREAL.IBN .\OBJ\WIN32\
 
@@ -58,7 +61,7 @@ echo Compiling Game
 
 cd code
 
-::WMAKE WIN32=1
+WMAKE WIN32=1
 
 :: manual linking to avoid a link path corruption issue
 ::%UTILS%\nwlink name ra95.exe @win95.lnk
