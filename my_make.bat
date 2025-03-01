951bf2bd6dce1b80d93b553e7495e72a9c8e26af
tomsons26 <tomsons26lv@gmail.com>
2025-03-01 19:05:16 +0200

    gets vqm32/vqa32 to compile

--- my_make.bat
+++ my_make.bat
@@ -7,10 +7,13 @@ SET UTILS=D:\Projects\utils\utils
 SET MASM=D:\MASM_6_11D
 
 REM Define Westwood libraries
+SET WIN32LIB=%ROOT%\WIN32LIB
+:: doesn't actually matter, needs to be removed from makefiles
+SET WIN32VCS=%ROOT%\WIN32LIB
 
 REM 3rd party libraries
 
-SET PATH=%WATCOM%\BINNT;%PATH%
+SET PATH=%WATCOM%\BINNT;%WATCOM%\H;%WATCOM%\H\NT;%PATH%
 SET PATH=%TASM%\BIN;%PATH%
 
 md OBJ\WIN32\ENGLISH
@@ -20,12 +23,42 @@ md RUN\ENGLISH
 md RUN\FRENCH
 md RUN\GERMAN
 
+md WINVQ\VQM32\O
+md WINVQ\VQA32\O
+
 ::copy /Y %IPX%\IPXREAL.IBN .\OBJ\WIN32\
+
+
+echo Compiling VQA
+
+cd winvq
+
+cd vqm32
+
+WMAKE
+
+cd ..
+
+cd vqa32
+
+WMAKE
+
+cd ..
+cd ..
+
+echo Compiling WWLIB
+
+cd win32lib
+
+WMAKE
+
+cd ..
+
+echo Compiling Game
 
 cd code
 
-WMAKE WIN32=1
-::WMAKE WIN32=1 /a
+::WMAKE WIN32=1
 
 :: manual linking to avoid a link path corruption issue
 ::%UTILS%\nwlink name ra95.exe @win95.lnk
